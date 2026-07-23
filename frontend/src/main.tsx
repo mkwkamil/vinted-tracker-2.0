0ebb6aa4c85088695718e064e78ac83dcf3eb394
mkwkamil <kamil.porebski@icloud.com>
2026-07-23 19:48:22 +0200

    vm update

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import App from './App'
+import { ToastProvider } from './components/Toast'
 import { ENABLE_VINTED } from './config/features'
 import MapPage from './pages/Map'
 import TrackerPage from './pages/Tracker'
@@ -9,23 +10,25 @@ import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<App />}>
-          {ENABLE_VINTED ? (
-            <>
-              <Route path="/" element={<TrackerPage />} />
-              <Route path="/map" element={<MapPage />} />
-            </>
-          ) : (
-            <>
-              <Route path="/" element={<MapPage />} />
-              <Route path="/map" element={<MapPage />} />
-              <Route path="*" element={<Navigate to="/" replace />} />
-            </>
-          )}
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ToastProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<App />}>
+            {ENABLE_VINTED ? (
+              <>
+                <Route path="/" element={<TrackerPage />} />
+                <Route path="/map" element={<MapPage />} />
+              </>
+            ) : (
+              <>
+                <Route path="/" element={<MapPage />} />
+                <Route path="/map" element={<MapPage />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
+              </>
+            )}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ToastProvider>
   </StrictMode>,
 )
